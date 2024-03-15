f8e3e2e30e9a0e91a81e787496f2a9cfa58060ae
GarimaVerma <garimaverma449@gmail.com>
2024-03-15 22:18:17 +0530

    Adding files to github

---
+++ Room Retreat/client/src/components/Categories.jsx
@@ -0,0 +1,32 @@
+import { categories } from "../data";
+import "../styles/Categories.scss"
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <h1>Explore the wide range of Rooms !
+      </h1>
+      <p>
+      Find your ideal room rental effortlessly with our platform. Browse diverse listings, filter by preferences, and connect directly with landlords. Simplify your search and secure your perfect space today.
+      </p>
+
+      <div className="categories_list">
+        {categories?.slice(1, 7).map((category, index) => (
+          <Link to={`/properties/category/${category.label}`}>
+            <div className="category" key={index}>
+              <img src={category.img} alt={category.label} />
+              <div className="overlay"></div>
+              <div className="category_text">
+                <div className="category_text_icon">{category.icon}</div>
+                <p>{category.label}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ Room Retreat/client/src/components/Slide.jsx
@@ -0,0 +1,13 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+      Find comfort in every corner with our affordable room rentals. 
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
